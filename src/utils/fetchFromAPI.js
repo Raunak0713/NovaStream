4954ac9c6a588bfb086a33fe654d9f37a3d2e442
Raunak0713 <raunakbhanarkar7@gmail.com>
2024-02-24 12:26:23 +0530

    first commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': 'a9906a45a1mshf6ce0574ca3dd48p177c7cjsne35a073e2d9f',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
